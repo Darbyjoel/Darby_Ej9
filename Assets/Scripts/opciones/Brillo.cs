a71167e8869e24e515e0a3d29b8a36025c58024c
Darby Joel <darbycabanillaesp@gmail.com>
2026-05-04 04:42:56 +0200

    vERSION CON OPTION AVANZANDIO
    eL OPTION YA TIENE UN SCRIOPS FUNCIONAL

--- Assets/Scripts/opciones/Brillo.cs
+++ Assets/Scripts/opciones/Brillo.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,6 +6,32 @@ public class Brillo : MonoBehaviour
 {
     public Image Panel_Brillo;
     public Slider Slider_Brillo;
+    public GameObject Text_Brillo;
+    public GameObject Text_Volumen;
+
+
+
+    
+    public void Pantalla_BrilloEntra(GameObject panelAActivar)
+    {
+        Text_Volumen.SetActive(false);
+        Text_Brillo.SetActive(false); 
+
+        Text_Volumen.SetActive(true);
+    }
+
+    public GameObject[] todosLosPaneles; 
+
+    public void AbrirPanelEspecifico(int indice)
+    {
+        for (int i = 0; i < todosLosPaneles.Length; i++)
+        {
+          
+            todosLosPaneles[i].SetActive(i == indice);
+        }
+    }
+
+
 
     void Start()
     {
@@ -15,14 +42,20 @@ public class Brillo : MonoBehaviour
     }
 
 
+
     public void CambiarBrillo(float valor)
 
     {
         Color c = Panel_Brillo.color;
-        c.a = 1f - valor/10;
+        c.a = 1f - valor / 10;
         Panel_Brillo.color = c;
         PlayerPrefs.SetFloat("Brillo", valor);
         PlayerPrefs.Save();
-    }
 
+    }
+   
+    
 }
+   
+
+
